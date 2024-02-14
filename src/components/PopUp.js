7674504a21a8cad07bf06e643a34924a7a15dfce
enasMetaly <enasmetwally99@gmail.com>
2024-02-14 19:09:35 +0200

    create popup  component

---
+++ src/components/PopUp.js
@@ -0,0 +1,28 @@
+import PopUpThread from "./PopUpThread";
+import Threads from "./Thread";
+import ThreadInput from "./ThreadInput";
+
+
+const PopUp=({ user,setOpenPopUp,   popUpFeedThreads,text,setText,postThread})=> {
+    return (
+      <div className="popup">
+        <p onClick={()=>setOpenPopUp(false)}>X</p>
+        {popUpFeedThreads?.map( popUpFeedThread => <PopUpThread
+                                key={popUpFeedThread.id}
+                                popUpFeedThread={popUpFeedThread}
+                                                    
+          
+          
+                                       />)}
+        <ThreadInput
+          user={user}
+          text={text}
+          setText={setText}
+          postThread={postThread}
+        />
+      </div>
+    );
+  }
+  
+  export default PopUp;
+  
